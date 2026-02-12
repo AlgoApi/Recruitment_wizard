e51b1042abf988936bad1e47e3c40da9fc0a7186
tamelaos <tamelaos@student.21-school.ru>
2026-02-12 20:27:15 +0500

    bug fix

--- src/bot/services/crm_service.py
+++ src/bot/services/crm_service.py
@@ -60,7 +60,6 @@ async def auth_with_csrf(
         "Referer": "https://huntmecrm.com/login",
         "Upgrade-Insecure-Requests": "1",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
-        "X-Requested-With": "XMLHttpRequest",
         "Content-Type": "application/x-www-form-urlencoded"
     }
 
